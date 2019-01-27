a29b16364d42dcf74931d193439956ac6a5e1de6
Joryun <me@joryun.com>
2019-01-27 10:57:00 +0800

    feat: add CORS configuration

--- src/main/java/com/joryun/boot/scaffold/config/MvcConfiguration.java
+++ src/main/java/com/joryun/boot/scaffold/config/MvcConfiguration.java
@@ -1,5 +1,7 @@
 package com.joryun.boot.scaffold.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,10 +11,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author joryun
  * @date 2018-11-27
  */
+@Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                .maxAge(3600);
     }
 }
